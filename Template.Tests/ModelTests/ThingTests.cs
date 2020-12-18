c5e690f01d527b02abb5c04a1d279b59b45cff41
Chelsea Becker <cschweig2@gmail.com>
2020-12-18 07:56:09 -0800

    add tests folder with boilerplate

---
+++ Template.Tests/ModelTests/ThingTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToDoList.Models;
+using System;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class ItemTests : IDisposable
+    {
+        public void Dispose()
+        {
+        Item.ClearAll();
+        }
+    }
+}
