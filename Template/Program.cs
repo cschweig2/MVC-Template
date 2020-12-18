e56da3c53b9b831dbf3221065017783253970df7
Chelsea Becker <cschweig2@gmail.com>
2020-12-18 07:54:13 -0800

    reformat spacing

--- Template/Program.cs
+++ Template/Program.cs
@@ -2,18 +2,19 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Template
-  public class Program
-  {
-    public static void Main(string[] args)
+{
+    public class Program
     {
-      var host = new WebHostBuilder()
-        .UseKestrel()
-        .UseContentRoot(Directory.GetCurrentDirectory())
-        .UseIISIntegration()
-        .UseStartup<Startup>()
-        .Build();
+        public static void Main(string[] args)
+        {
+        var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
 
-      host.Run();
+        host.Run();
+        }
     }
-  }
 }
